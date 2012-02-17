65ed2afef8fa73cb6f8d377b63ba69d8a9c75b3c
Jeremy Spitzig <jspitzig@cardinalsolutions.com>
2012-02-16 20:01:20 -0500

    Added help page and refactored title unit tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe PagesController do
-render_views
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -10,7 +14,7 @@ render_views
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home");
+      response.should have_selector("title", :content => @base_title + "Home")
     end
   end
 
@@ -21,7 +25,7 @@ render_views
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + "Contact")
     end
   end
 
@@ -32,7 +36,18 @@ render_views
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + "About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + "Help")
     end
   end
 end
