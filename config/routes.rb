9f91e9299ecc97dcda3a1d0fd977b863b4ca48b1
Jeremy Spitzig <jspitzig@cardinalsolutions.com>
2012-02-17 22:27:48 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -7,6 +9,7 @@ SampleApp::Application.routes.draw do
 
   get "pages/help"
 
+  match '/signup', :to=>'users#new'
   match '/contact', :to=>'pages#contact'
   match '/about', :to=>'pages#about'
   match '/help', :to=>'pages#help'
